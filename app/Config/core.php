e279a39507243f7aa75c633d49137d4948d965da
nanashiRei <nanashi.rei@gmail.com>
2013-01-15 11:28:19 +0100

    kk

--- app/Config/core.php
+++ app/Config/core.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This is core configuration file.
  *
@@ -186,12 +187,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'Q$66REh&&sjRZ7jRsJZShRH&tHUTJR&SR6hSRtHR&WETHRh');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '84938475349756298307430942383');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
