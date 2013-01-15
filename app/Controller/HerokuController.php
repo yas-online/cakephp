3a9b8f2f0874bb304c077f33df4d3281598e421d
nanashiRei <nanashi.rei@gmail.com>
2013-01-15 14:15:04 +0100

    kk2nd

---
+++ app/Controller/HerokuController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * YaS-Online.net - Yet another Server Online
+ * @link http://cc.yas-online.net/
+ *
+ * @author "David Marner" <nanashi.rei@gmail.com>
+ * @created 15.01.13 - 11:52
+ *
+ * This file is licensed under a
+ * Creative Commons Attribution-ShareAlike 3.0 Germany License.
+ * You can view an online copy here:
+ * @link http://creativecommons.org/licenses/by-sa/3.0/de/deed.en
+ */
+class HerokuController extends AppController {
+
+    public function indexAction() {
+        //...
+    }
+
+}
